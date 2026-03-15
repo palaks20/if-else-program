cff6ee3d188fd4d85ebd7957d7baf80ddedd9784
palaks20 <singhpalak3012@gmail.com>
2026-03-15 14:40:00 +0530

    Create vote.java
    Added  voting eligibility program

---
+++ vote.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Vote {
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System in);
+    int age;
+    System.out.println("Enter age:");
+    age = sc.nextInt();
+    if(age >= 18)
+      System.out.println("Eligible to vote");
+    else
+      System.out.println("Not eligible to vote");
+  }
+}
